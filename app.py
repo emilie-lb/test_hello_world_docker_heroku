260e5bb78adf8c47068f27cfb66a4e6a56ea56d3
emilie-lb <emilielb75@gmail.com>
2021-05-19 19:22:52 +0200

    rectification du port dans app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', )
 def index():
   return "Hello, ceci est un test pour voir si le workflow fonctionne"
 
@@ -14,4 +14,4 @@ def hello_world():
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
-  app.run(debug = True, host = '0.0.0.0')
+  app.run(host = '0.0.0.0', port = port)
